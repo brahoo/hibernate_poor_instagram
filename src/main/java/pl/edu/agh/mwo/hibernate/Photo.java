0ea2bbb705b59f2a1a6ff576af5111bf6d8869d9
Piotrek <piotr.pogorzelec@gazeta.pl>
2022-02-07 19:47:51 +0100

    Photo basic implementation

---
+++ src/main/java/pl/edu/agh/mwo/hibernate/Photo.java
@@ -0,0 +1,34 @@
+package pl.edu.agh.mwo.hibernate;
+
+import java.time.LocalDate;
+
+public class Photo {
+
+    private long id;
+    private String name;
+    private LocalDate date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
