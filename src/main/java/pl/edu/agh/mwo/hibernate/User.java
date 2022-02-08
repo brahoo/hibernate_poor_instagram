7c269fca483ebf9b33427beb33662fb15cba0aec
Piotrek <piotr.pogorzelec@gazeta.pl>
2022-02-08 18:56:59 +0100

    User relational mapping

--- src/main/java/pl/edu/agh/mwo/hibernate/User.java
+++ src/main/java/pl/edu/agh/mwo/hibernate/User.java
@@ -1,16 +1,30 @@
 package pl.edu.agh.mwo.hibernate;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column
     private String username;
+
+    @Column
     private LocalDate joinDate;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
     private Set<Album> albums = new HashSet<>();
+
     private Set<Photo> likedPhotos = new HashSet<>();
+
 
     public long getId() {
         return id;
